702019edab0a6f7dfe3c5c6b5643a98cf48908be
Ashish Sam T George <ashishsamtgeorge@mca.ajce.in>
2023-07-19 16:48:12 +0530

    Create Mutation for adding new education

--- Tithe-Spring/src/main/java/com/tithe/controller/mutation/PersonMutations.java
+++ Tithe-Spring/src/main/java/com/tithe/controller/mutation/PersonMutations.java
@@ -3,13 +3,17 @@
  */
 package com.tithe.controller.mutation;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
 
+import com.tithe.entity.EducationEntity;
 import com.tithe.entity.PersonEntity;
 import com.tithe.model.PersonMutationInput;
+import com.tithe.service.mutation.EducationMutationService;
 import com.tithe.service.mutation.PersonMutationService;
 
 /**
@@ -21,12 +25,18 @@ public class PersonMutations {
 	
 	@Autowired
 	private PersonMutationService personMutationService;
+	
+	@Autowired
+	private EducationMutationService educationMutationService;
 
 	@MutationMapping(name = "createOnePerson")
 	public PersonEntity createOnePerson(@Argument PersonMutationInput personMutationInput) {
 		return personMutationService.createOnePerson(personMutationInput);
 	}
 	
-	
+	@MutationMapping(name = "createManyEducations")
+	public List<EducationEntity> createManyEducations(List<String> educationNames) {
+		return educationMutationService.createManyEducations(educationNames);
+	}
 	
 }
